3f34da8e0bb730719e83720076d20e271cffc21d
Thiago Tietjen <thiago_tietjen@estudante.sesisenai.org.br>
2024-09-26 20:04:13 -0300

    adicionado app inventario
    criada a pagina de criação de item e atualização do mesmo, faltando apenas arrumar a aparencia e funcionalidade

--- epi_shop/urls.py
+++ epi_shop/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.urls import path, include
+from inventario import views
 
 urlpatterns = [
-    path('', include('epi_shops.urls')),
+    path('', views.index, name='index'),
 ]
